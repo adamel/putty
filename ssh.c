0d3b586fed120a6bba4dc2036434366ca8bfcc4e
Marcus Sundberg <marcus@marcussundberg.com>
2013-10-06 16:47:16 +0200

    Make authentication gss_ctx global.
    Prepare for GSS key exchange.

--- ssh.c
+++ ssh.c
@@ -960,6 +960,7 @@ struct ssh_tag {
     struct ssh_gss_liblist *gsslibs;
     struct ssh_gss_library *gsslib;
     Ssh_gss_name gss_srv_name;
+    Ssh_gss_ctx gss_ctx;
     int can_gssapi;
 #endif
 };
@@ -8787,7 +8788,6 @@ static void do_ssh2_authconn(Ssh ssh, unsigned char *in, int inlen,
 	struct Packet *pktout;
 	Filename *keyfile;
 #ifndef NO_GSSAPI
-	Ssh_gss_ctx gss_ctx;
 	Ssh_gss_buf gss_buf;
 	Ssh_gss_buf gss_rcvtok, gss_sndtok;
 	Ssh_gss_stat gss_stat;
@@ -9641,7 +9641,7 @@ static void do_ssh2_authconn(Ssh ssh, unsigned char *in, int inlen,
 
 		/* fetch TGT into GSS engine */
 		s->gss_stat = ssh->gsslib->acquire_cred(ssh->gsslib,
-							&s->gss_ctx);
+							&ssh->gss_ctx);
 
 		if (s->gss_stat != SSH_GSS_OK) {
 		    logevent("GSSAPI authentication failed to get credentials");
@@ -9656,7 +9656,7 @@ static void do_ssh2_authconn(Ssh ssh, unsigned char *in, int inlen,
 		do {
 		    s->gss_stat = ssh->gsslib->init_sec_context
 			(ssh->gsslib,
-			 &s->gss_ctx,
+			 &ssh->gss_ctx,
 			 ssh->gss_srv_name,
 			 conf_get_int(ssh->conf, CONF_gssapifwd),
 			 &s->gss_rcvtok,
@@ -9666,7 +9666,8 @@ static void do_ssh2_authconn(Ssh ssh, unsigned char *in, int inlen,
 			s->gss_stat!=SSH_GSS_S_CONTINUE_NEEDED) {
 			logevent("GSSAPI authentication initialisation failed");
 
-			if (ssh->gsslib->display_status(ssh->gsslib, s->gss_ctx,
+			if (ssh->gsslib->display_status(ssh->gsslib,
+							ssh->gss_ctx,
 							&s->gss_buf)
 			    == SSH_GSS_OK) {
 			    logevent(s->gss_buf.value);
@@ -9702,7 +9703,7 @@ static void do_ssh2_authconn(Ssh ssh, unsigned char *in, int inlen,
 		} while (s-> gss_stat == SSH_GSS_S_CONTINUE_NEEDED);
 
 		if (s->gss_stat != SSH_GSS_OK) {
-		    ssh->gsslib->release_cred(ssh->gsslib, &s->gss_ctx);
+		    ssh->gsslib->release_cred(ssh->gsslib, &ssh->gss_ctx);
 		    continue;
 		}
 		logevent("GSSAPI authentication loop finished OK");
@@ -9721,7 +9722,8 @@ static void do_ssh2_authconn(Ssh ssh, unsigned char *in, int inlen,
 		s->gss_buf.value = (char *)s->pktout->data + micoffset;
 		s->gss_buf.length = s->pktout->length - micoffset;
 
-		ssh->gsslib->get_mic(ssh->gsslib, s->gss_ctx, &s->gss_buf, &mic);
+		ssh->gsslib->get_mic(ssh->gsslib, ssh->gss_ctx, &s->gss_buf,
+				     &mic);
 		s->pktout = ssh2_pkt_init(SSH2_MSG_USERAUTH_GSSAPI_MIC);
 		ssh_pkt_addstring_start(s->pktout);
 		ssh_pkt_addstring_data(s->pktout, mic.value, mic.length);
@@ -9729,8 +9731,6 @@ static void do_ssh2_authconn(Ssh ssh, unsigned char *in, int inlen,
 		ssh->gsslib->free_mic(ssh->gsslib, &mic);
 
 		s->gotit = FALSE;
-
-		ssh->gsslib->release_cred(ssh->gsslib, &s->gss_ctx);
 		continue;
 #endif
 	    } else if (s->can_keyb_inter && !s->kbd_inter_refused) {
@@ -10763,6 +10763,7 @@ static const char *ssh_init(void *frontend_handle, void **backend_handle,
     ssh->gsslibs = NULL;
     ssh->gsslib = NULL;
     ssh->gss_srv_name = NULL;
+    ssh->gss_ctx = NULL;
 #endif
 
     random_ref(); /* do this now - may be needed by sharing setup code */
@@ -10886,6 +10887,8 @@ static void ssh_free(void *handle)
     sfree(ssh->username);
     conf_free(ssh->conf);
 #ifndef NO_GSSAPI
+    if (ssh->gss_ctx)
+	ssh->gsslib->release_cred(ssh->gsslib, &ssh->gss_ctx);
     if (ssh->gss_srv_name)
 	ssh->gsslib->release_name(ssh->gsslib, &ssh->gss_srv_name);
     if (ssh->gsslibs)
