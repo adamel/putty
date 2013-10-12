85026211174adcd1c17a5e49667dce1556772a2f
Marcus Sundberg <marcus@marcussundberg.com>
2013-10-12 23:26:30 +0200

    Use underscored variants of _stricmp and _strnicmp.
    The non-underscored variants are deprecated by Microsoft.

--- ssh.c
+++ ssh.c
@@ -178,15 +178,15 @@ static unsigned int ssh_tty_parse_specchar(char *s)
 }
 static unsigned int ssh_tty_parse_boolean(char *s)
 {
-    if (stricmp(s, "yes") == 0 ||
-	stricmp(s, "on") == 0 ||
-	stricmp(s, "true") == 0 ||
-	stricmp(s, "+") == 0)
+    if (_stricmp(s, "yes") == 0 ||
+	_stricmp(s, "on") == 0 ||
+	_stricmp(s, "true") == 0 ||
+	_stricmp(s, "+") == 0)
 	return 1; /* true */
-    else if (stricmp(s, "no") == 0 ||
-	     stricmp(s, "off") == 0 ||
-	     stricmp(s, "false") == 0 ||
-	     stricmp(s, "-") == 0)
+    else if (_stricmp(s, "no") == 0 ||
+	     _stricmp(s, "off") == 0 ||
+	     _stricmp(s, "false") == 0 ||
+	     _stricmp(s, "-") == 0)
 	return 0; /* false */
     else
 	return (atoi(s) != 0);
