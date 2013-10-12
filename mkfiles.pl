b06e31fe04aa3e0f16a85a59fa0f1e517752dd7c
Marcus Sundberg <marcus@marcussundberg.com>
2013-10-12 21:07:49 +0200

    Do not define WIN32S_COMPAT in Makefile.cyg
    PuTTY code hasn't used it for 13 years...

--- mkfiles.pl
+++ mkfiles.pl
@@ -586,7 +586,7 @@ if (defined $makefiles{'cygwin'}) {
     "# You may also need to tell windres where to find include files:\n".
     "# RCINC = --include-dir c:\\cygwin\\include\\\n".
     "\n".
-    &splitline("CFLAGS = -Wall -O2 -D_WINDOWS -DDEBUG -DWIN32S_COMPAT".
+    &splitline("CFLAGS = -Wall -O2 -D_WINDOWS -DDEBUG".
       " -D_NO_OLDNAMES " .
 	       (join " ", map {"-I$dirpfx$_"} @srcdirs)) .
 	       "\n".
